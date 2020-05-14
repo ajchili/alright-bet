927fef99cb1c93560f74faacbeaafece8de489d2
Kirin Patel <kirinpatel@gmail.com>
2020-05-13 23:21:31 -0400

    Added port specification support via environment variables

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
+const PORT: string = process.env.PORT || "80";
 
 app.get("/", (_: Request, res: Response) => {
   res.status(200).send("Hello, World!");
 });
 
-app.listen(8080);
+app.listen(PORT);
