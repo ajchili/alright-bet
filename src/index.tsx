98f6822529633d2122b46a68437945cb6a290f33
Kirin Patel <kirinpatel@gmail.com>
2020-05-18 16:14:53 -0400

    windowData -> window

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 
 // @ts-ignore
-const { __INITIAL__DATA__: data = {} } = windowData;
+const { __INITIAL__DATA__: data = {} } = window;
 
 ReactDOM.hydrate(
   <React.StrictMode>
