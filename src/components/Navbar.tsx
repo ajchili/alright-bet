8b00bd0af959184dad21592366cf6660d9017f60
Kirin Patel <kirinpatel@gmail.com>
2020-05-23 10:56:07 -0400

    Added home button to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 import { Image, Menu } from "semantic-ui-react";
 import { User } from "../lib/v1";
 
@@ -19,11 +20,16 @@ export default class extends Component<Props> {
               avatar />
           </Menu.Item>
         }
+        <Menu.Item>
+          <Link to="/">
+            Home
+          </Link>
+        </Menu.Item>
         <Menu.Menu position='right'>
           <Menu.Item>
             <a href="/api/v1/authentication/logout">
               Logout
-              </a>
+            </a>
           </Menu.Item>
         </Menu.Menu>
       </Menu>
