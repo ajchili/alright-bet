dba48bff92a67405196b1e33ee980f218871702e
Kirin Patel <kirinpatel@gmail.com>
2020-05-23 01:25:58 -0400

    find -> get

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,7 +2,7 @@ import { DiscordUser, User } from "../lib/v1";
 import { users } from "./database";
 
 export const findAndUpdateOrCreate = async (user: DiscordUser | User) => {
-  const existingUser = await users.find(user.id);
+  const existingUser = await users.get(user.id);
   if (existingUser) {
     existingUser.lastLoginTime = new Date().getTime();
     await users.update(existingUser);
