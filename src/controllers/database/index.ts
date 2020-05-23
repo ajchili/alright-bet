abc7737cfa1ef2b44edb78ad304ae114f6b44823
Kirin Patel <kirinpatel@gmail.com>
2020-05-23 14:15:37 -0400

    Fixed database seeding issue

--- src/controllers/database/index.ts
+++ src/controllers/database/index.ts
@@ -1,10 +1,9 @@
-import { Table } from "../../lib/v1";
 import * as Bets from "./bets";
 import * as Groups from "./groups";
 import * as Members from "./members";
 import * as Roles from "./roles";
 import * as Users from "./users";
-import { createTable } from "./utils";
+import seedDatabase from "./seed";
 
 export const bets = {
   ...Bets
@@ -23,101 +22,6 @@ export const users = {
 };
 
 export const seed = async (): Promise<void> => {
-  const createTableAndThrowUnknownErr = async (table: Table) => {
-    try {
-      await createTable(table);
-    } catch (err) {
-      switch (err.code) {
-        // "relation already exists" error, ignore if caught
-        case "42P07":
-          break;
-        default:
-          throw err;
-      }
-    }
-  };
-  await createTableAndThrowUnknownErr({
-    name: "users",
-    rows: [
-      {
-        name: "id",
-        type: "VARCHAR (100)",
-        primaryKey: true,
-        unique: true,
-      },
-      {
-        name: "username",
-        type: "VARCHAR (32)",
-      },
-      {
-        name: "avatar",
-        type: "VARCHAR (255)",
-      },
-      {
-        name: "discriminator",
-        type: "VARCHAR (4)",
-      },
-    ],
-  });
-  await createTableAndThrowUnknownErr({
-    name: "groups",
-    rows: [
-      {
-        name: "id",
-        type: "SERIAL",
-        primaryKey: true,
-        unique: true,
-      },
-      {
-        name: "name",
-        type: "VARCHAR (100)",
-      },
-    ],
-  });
-  await createTableAndThrowUnknownErr({
-    name: "roles",
-    rows: [
-      {
-        name: "id",
-        type: "SERIAL",
-        primaryKey: true,
-        unique: true,
-      },
-      {
-        name: "name",
-        type: "VARCHAR (100)",
-      },
-    ],
-  });
-  await createTableAndThrowUnknownErr({
-    name: "members",
-    rows: [
-      {
-        name: "id",
-        type: "SERIAL",
-        primaryKey: true,
-        unique: true,
-      },
-      {
-        name: "user_id",
-        type: "VARCHAR (100)",
-        references: "users(id)",
-      },
-      {
-        name: "group_id",
-        type: "INTEGER",
-        references: "groups(id)",
-      },
-      {
-        name: "role_id",
-        type: "INTEGER",
-        references: "roles(id)",
-      },
-      {
-        name: "currency",
-        type: "MONEY",
-      },
-    ],
-  });
+  await seedDatabase();
   await Roles.seed();
 };
