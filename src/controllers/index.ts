80db00ba0baedd3b30752d90547dadfe6b74cd7c
Kirin Patel <kirinpatel@gmail.com>
2020-05-29 23:10:21 -0400

    Added exports for controllers folder

---
+++ src/controllers/index.ts
@@ -0,0 +1,6 @@
+export * as Bets from "./bets";
+export * as Database from "./database";
+export * as Groups from "./groups";
+export * as Members from "./members";
+export * as Users from "./users";
+export * as Wagers from "./wagers";
