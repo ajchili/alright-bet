02bae29ab16497c1401f44bafcadc116cffe0fce
Kirin Patel <kirinpatel@gmail.com>
2020-05-30 16:46:01 -0400

    Created controller functions to get detailed user data

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,5 +1,8 @@
-import { DiscordUser, User } from "../lib/v1";
+import { QueryResult } from "pg";
+import { DetailedUser, DiscordUser, User } from "../lib/v1";
 import { users } from "./database";
+import { getClient } from "./database/utils";
+import * as Members from "./members";
 
 export const findAndUpdateOrCreate = async (user: DiscordUser | User) => {
   const existingUser = await users.find(user.id);
@@ -12,4 +15,28 @@ export const findAndUpdateOrCreate = async (user: DiscordUser | User) => {
 
 export const find = async (id: string): Promise<User> => {
   return await users.find(id);
+};
+
+export const getDetailedData = async (id: string): Promise<DetailedUser> => {
+  const client = await getClient();
+  const user: User = await new Promise((resolve, reject) => {
+    client.query(
+      "SELECT * FROM users WHERE id = $1",
+      [id],
+      (err: Error, result: QueryResult) => {
+        client.release(true);
+        if (err) {
+          reject(err);
+        } else if (result.rowCount === 0) {
+          reject(new Error("User does not exist!"));
+        }
+        resolve(result.rows[0] as User);
+      }
+    );
+  });
+  const memberships = await Members.getForUser(user);
+  return {
+    ...user,
+    memberships,
+  };
 };
