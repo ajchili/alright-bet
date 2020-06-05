5c83bb69465635a1df6c319bd3b38bc0d278cc33
Kirin Patel <kirinpatel@gmail.com>
2020-06-05 11:40:47 -0400

    Removed seed code from server

--- src/controllers/database/index.ts
+++ src/controllers/database/index.ts
@@ -5,7 +5,6 @@ import * as Roles from "./roles";
 import * as Users from "./users";
 import * as Utils from "./utils";
 import * as Wagers from "./wagers";
-import seedDatabase from "./seed";
 
 export const bets = {
   ...Bets
@@ -28,8 +27,3 @@ export const wagers = {
 };
 
 export const getPool = Utils.getPool;
-
-export const seed = async (): Promise<void> => {
-  await seedDatabase();
-  await Roles.seed();
-};
--- src/server.ts
+++ src/server.ts
@@ -13,36 +13,34 @@ import GroupsRouter from "./routes/v1/groups";
 import ReactRouter from "./routes/v1/react";
 import AuthenticationMiddleware from "./middleware/authentication";
 import * as Routes from "./routes";
-import { seed, getPool } from "./controllers/database";
+import { getPool } from "./controllers/database";
 
-seed().then(() => {
-  const pgSession = connectPgSimple(session);
+const pgSession = connectPgSimple(session);
 
-  const app: Express = express();
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-  app.use(cookieParser());
-  app.use(
-    session({
-      cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
-      resave: false,
-      saveUninitialized: false,
-      secret: process.env.SESSION_SECRET,
-      store: new pgSession({
-        pool: getPool(),
-      }),
-    })
-  );
-  app.use(express.static("build", { index: false }));
-  const PORT: string = process.env.PORT || "80";
+const app: Express = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(
+  session({
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.SESSION_SECRET,
+    store: new pgSession({
+      pool: getPool(),
+    }),
+  })
+);
+app.use(express.static("build", { index: false }));
+const PORT: string = process.env.PORT || "80";
 
-  app.use(AuthenticationMiddleware);
+app.use(AuthenticationMiddleware);
 
-  app.use("/api/v1/authentication", AuthenticationRouter);
-  app.use("/api/v1/bets", BetsRouter);
-  app.use("/api/v1/groups", GroupsRouter);
-  app.get("/api/v1/users/:id", Routes.v1.Users.getDetailedData);
-  app.use("/", ReactRouter);
+app.use("/api/v1/authentication", AuthenticationRouter);
+app.use("/api/v1/bets", BetsRouter);
+app.use("/api/v1/groups", GroupsRouter);
+app.get("/api/v1/users/:id", Routes.v1.Users.getDetailedData);
+app.use("/", ReactRouter);
 
-  app.listen(PORT);
-});
+app.listen(PORT);
